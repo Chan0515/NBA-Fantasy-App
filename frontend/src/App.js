a63506fe4e588367ddadb1fb1f7db6880fa4ecd9
Chan0515 <32955432+Chan0515@users.noreply.github.com>
2022-05-02 22:26:10 -0700

    Updated build files.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,6 @@
 import React from 'react';
-import { Routes, Route } from 'react-router-dom';
 import Navbar from './components/Navbar/Navbar';
 import NBA from './NBA';
-import Home from './Home';
 function App() {
     return(
         <div>
